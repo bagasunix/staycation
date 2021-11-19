2ab5a730009c90e276db0950d12655a1312960ab
Aldino Pratama <aldinopratama15@gmail.com>
2021-11-20 00:43:24 +0700

    set ejs layout

--- adminpanel/app.js
+++ adminpanel/app.js
@@ -4,6 +4,8 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const mongoose = require('mongoose')
+const ejsLayouts = require('express-ejs-layouts');
+
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
@@ -25,8 +27,11 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/bagasunix', express.static(path.join(__dirname, 'node_modules/admin-lte')))
-app.set('views', './views') // specify the views directory
+app.set('layout', './index') // specify the views directory
 app.set('view engine', 'ejs') // register the template engine
+app.set('layout extractScripts', true)
+app.set('layout extractStyles', true)
+app.use(ejsLayouts);
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
